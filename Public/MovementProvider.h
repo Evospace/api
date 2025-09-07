b9e9506930dc9f149b767e6f379d22e24004b330
wavelet-noise <ishellstrike@gmail.com>
2025-09-07 18:25:21 +0300

    Create MovementProvider.h

---
+++ Public/MovementProvider.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "CoreMinimal.h"
+
+#include "MovementProvider.generated.h"
+
+UINTERFACE(NotBlueprintable)
+class UMovementProvider : public UInterface {
+  GENERATED_BODY()
+};
+
+class IMovementProvider {
+  GENERATED_BODY()
+
+  public:
+
+  UFUNCTION(BlueprintCallable)
+  virtual void EnableMovement() PURE_VIRTUAL(UMovementProvider::EnableMovement, );
+
+  UFUNCTION(BlueprintCallable)
+  virtual void DisableMovement() PURE_VIRTUAL(UMovementProvider::DisableMovement, );
+};
