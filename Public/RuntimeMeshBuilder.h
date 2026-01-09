aeec5e6c6e58d315def3ed74e9d79acbbdec64a6
wavelet-noise <ishellstrike@gmail.com>
2026-01-09 16:01:28 +0300

    cube tesselator fix

--- Public/RuntimeMeshBuilder.h
+++ Public/RuntimeMeshBuilder.h
@@ -8,27 +8,68 @@ struct RuntimeMeshBuilder {
   using MeshType = RealtimeMesh::TRealtimeMeshBuilderLocal<uint32, FPackedNormal, FVector2DHalf, 1>;
 
   private:
-  static int32 BuildSection(MeshType &StaticProvider, const UTesselator::Data &data, int32 section_index,
-                            int32 precount) {
-    auto &index_data = data[section_index];
-
-    for (int i = 0; i < data[section_index].Vertices.Num(); ++i) {
-      StaticProvider.AddVertex(index_data.Vertices[i])
-        .SetNormalAndTangent(index_data.Normals[i], index_data.Tangents[i])
-        .SetTexCoord(FVector2DHalf(index_data.UV0[i].X, index_data.UV0[i].Y))
-        .SetColor(index_data.VertexColors[i]);
+  static int32 BuildSection(MeshType &StaticProvider, const UTesselator::MeshData &SectionData, int32 SectionIndex,
+                            int32 VertexBase) {
+    if (SectionData.Normals.Num() != SectionData.Vertices.Num()) {
+      LOG(ERROR_LL) << "RuntimeMeshBuilder: Normals mismatch: Vertices=" << SectionData.Vertices.Num()
+                    << " Normals=" << SectionData.Normals.Num()
+                    << " Section=" << SectionIndex;
+      return 0;
+    }
+    if (SectionData.Tangents.Num() != SectionData.Vertices.Num()) {
+      LOG(ERROR_LL) << "RuntimeMeshBuilder: Tangents mismatch: Vertices=" << SectionData.Vertices.Num()
+                    << " Tangents=" << SectionData.Tangents.Num()
+                    << " Section=" << SectionIndex;
+      return 0;
+    }
+    if (SectionData.UV0.Num() != SectionData.Vertices.Num()) {
+      LOG(ERROR_LL) << "RuntimeMeshBuilder: UV0 mismatch: Vertices=" << SectionData.Vertices.Num()
+                    << " UV0=" << SectionData.UV0.Num()
+                    << " Section=" << SectionIndex;
+      return 0;
+    }
+    if (SectionData.VertexColors.Num() != SectionData.Vertices.Num()) {
+      LOG(ERROR_LL) << "RuntimeMeshBuilder: VertexColors mismatch: Vertices=" << SectionData.Vertices.Num()
+                    << " Colors=" << SectionData.VertexColors.Num()
+                    << " Section=" << SectionIndex;
+      return 0;
+    }
+    if (SectionData.Triangles.Num() % 3 != 0) {
+      LOG(ERROR_LL) << "RuntimeMeshBuilder: Triangles not multiple of 3: Triangles=" << SectionData.Triangles.Num()
+                    << " Section=" << SectionIndex;
+      return 0;
     }
 
-    for (int i = 0; i < index_data.Triangles.Num(); i += 3) {
-      StaticProvider.AddTriangle(index_data.Triangles[i] + precount, index_data.Triangles[i + 1] + precount, index_data.Triangles[i + 2] + precount, section_index);
+    for (int32 i = 0; i < SectionData.Vertices.Num(); ++i) {
+      StaticProvider.AddVertex(SectionData.Vertices[i])
+        .SetNormalAndTangent(SectionData.Normals[i], SectionData.Tangents[i])
+        .SetTexCoord(FVector2DHalf(SectionData.UV0[i].X, SectionData.UV0[i].Y))
+        .SetColor(SectionData.VertexColors[i]);
     }
 
-    return data[section_index].Vertices.Num();
+    for (int32 i = 0; i < SectionData.Triangles.Num(); i += 3) {
+      StaticProvider.AddTriangle(
+        SectionData.Triangles[i] + VertexBase,
+        SectionData.Triangles[i + 1] + VertexBase,
+        SectionData.Triangles[i + 2] + VertexBase,
+        SectionIndex);
+    }
+
+    return SectionData.Vertices.Num();
   }
 
   static FRealtimeMeshSectionGroupKey MakeSurfaceGroupKey(int32 LodIndex) {
     static const FName SurfaceName(TEXT("Surface"));
     return FRealtimeMeshSectionGroupKey::Create(FRealtimeMeshLODKey(LodIndex), SurfaceName);
+  }
+
+  static bool HasSectionGroup(const URealtimeMeshSimple *Mesh, const FRealtimeMeshSectionGroupKey &GroupKey) {
+    if (!Mesh) {
+      return false;
+    }
+
+    const TArray<FRealtimeMeshSectionGroupKey> SectionGroups = Mesh->GetSectionGroups(GroupKey.LOD());
+    return SectionGroups.Contains(GroupKey);
   }
 
   static void EnsureLodExists(URealtimeMeshSimple *Mesh, int32 LodIndex) {
@@ -72,60 +113,60 @@ struct RuntimeMeshBuilder {
     Builder.EnableColors();
     Builder.EnablePolyGroups();
 
-    int32 section_count = 0;
-    for (size_t i = 0; i < data.Num(); ++i) {
-      if (data[i].Vertices.Num() > 0) {
-        if (ensure(data[i].material)) {
-          section_count += BuildSection(Builder, data, i, section_count);
-          const FString SlotNameString =
-            data[i].material->GetFName().ToString() + (data[i].SmoothNormals ? TEXT("_Smooth") : TEXT("_Flat"));
-          rm->SetupMaterialSlot(i, FName(*SlotNameString), data[i].material);
-        } else {
-          section_count += BuildSection(Builder, data, i, section_count);
-          const FString SlotNameString =
-            FString(TEXT("UnloadedMaterial")) + (data[i].SmoothNormals ? TEXT("_Smooth") : TEXT("_Flat"));
-          rm->SetupMaterialSlot(i, FName(*SlotNameString), nullptr);
-        }
-      }
-    }
-
-    // IMPORTANT:
-    // - Creating/updating section-group streams and updating section configs must be done in a single commit.
-    //   Otherwise, UpdateSectionConfig may run before the poly-group sections are created and triggers
-    //   `ensure(Section.IsValid())` inside RealtimeMeshComponent.
-    RealtimeMesh::FRealtimeMeshUpdateBuilder UpdateBuilder;
-
-    UpdateBuilder.AddLODTask<RealtimeMesh::FRealtimeMeshLODSimple>(
-      groupKey.LOD(),
-      [groupKey](RealtimeMesh::FRealtimeMeshUpdateContext &UpdateContext, RealtimeMesh::FRealtimeMeshLODSimple &LOD) {
-        LOD.CreateOrUpdateSectionGroup(UpdateContext, groupKey, FRealtimeMeshSectionGroupConfig());
-      });
-
-    // Copy once; SectionGroup::SetAllStreams expects an rvalue in this path.
-    RealtimeMesh::FRealtimeMeshStreamSet StreamSetCopy(StreamSet);
-    UpdateBuilder.AddSectionGroupTask<RealtimeMesh::FRealtimeMeshSectionGroupSimple>(
-      groupKey,
-      [StreamSetCopy = MoveTemp(StreamSetCopy)](RealtimeMesh::FRealtimeMeshUpdateContext &UpdateContext,
-                                                RealtimeMesh::FRealtimeMeshSectionGroupSimple &SectionGroup) mutable {
-        SectionGroup.SetShouldAutoCreateSectionsForPolyGroups(UpdateContext, true);
-        SectionGroup.SetAllStreams(UpdateContext, MoveTemp(StreamSetCopy));
-      });
-
-    for (size_t i = 0; i < data.Num(); ++i) {
-      if (data[i].Triangles.Num() == 0) {
+    TSet<int32> ActivePolyGroups;
+    int32 VertexBase = 0;
+    for (int32 SectionIndex = 0; SectionIndex < data.Num(); ++SectionIndex) {
+      const auto &SectionData = data[SectionIndex];
+      if (SectionData.Vertices.Num() == 0) {
         continue;
       }
 
-      const FRealtimeMeshSectionKey PolyGroupKey = FRealtimeMeshSectionKey::CreateForPolyGroup(groupKey, i);
-      const FRealtimeMeshSectionConfig Config(static_cast<int32>(i));
+      if (SectionData.material) {
+        rm->SetupMaterialSlot(SectionIndex, SectionData.material->GetFName(), SectionData.material);
+      } else {
+        rm->SetupMaterialSlot(SectionIndex, "UnloadedMaterial", nullptr);
+      }
 
-      UpdateBuilder.AddSectionTask<RealtimeMesh::FRealtimeMeshSectionSimple>(
-        PolyGroupKey,
-        [Config](RealtimeMesh::FRealtimeMeshUpdateContext &UpdateContext, RealtimeMesh::FRealtimeMeshSectionSimple &Section) {
-          Section.UpdateConfig(UpdateContext, Config);
-          Section.SetShouldCreateCollision(UpdateContext, true);
+      const int32 AddedVertexCount = BuildSection(Builder, SectionData, SectionIndex, VertexBase);
+      if (AddedVertexCount > 0 && SectionData.Triangles.Num() > 0) {
+        ActivePolyGroups.Add(SectionIndex);
+      }
+      VertexBase += AddedVertexCount;
+    }
+
+    RealtimeMesh::FRealtimeMeshUpdateBuilder UpdateBuilder;
+
+    const bool bHasSectionGroup = HasSectionGroup(rm, groupKey);
+    if (!bHasSectionGroup) {
+      UpdateBuilder.AddLODTask<RealtimeMesh::FRealtimeMeshLODSimple>(groupKey,
+        [groupKey](RealtimeMesh::FRealtimeMeshUpdateContext &UpdateContext, RealtimeMesh::FRealtimeMeshLODSimple &LOD) {
+          LOD.CreateOrUpdateSectionGroup(UpdateContext, groupKey, FRealtimeMeshSectionGroupConfig());
         });
     }
+
+    UpdateBuilder.AddSectionGroupTask<RealtimeMesh::FRealtimeMeshSectionGroupSimple>(groupKey,
+      [StreamSet = MoveTemp(StreamSet)](RealtimeMesh::FRealtimeMeshUpdateContext &UpdateContext,
+                                        RealtimeMesh::FRealtimeMeshSectionGroupSimple &SectionGroup) mutable {
+        SectionGroup.SetShouldAutoCreateSectionsForPolyGroups(UpdateContext, true);
+        SectionGroup.SetAllStreams(UpdateContext, MoveTemp(StreamSet));
+      });
+
+    UpdateBuilder.AddSectionGroupTask<RealtimeMesh::FRealtimeMeshSectionGroupSimple>(groupKey,
+      [ActivePolyGroups = MoveTemp(ActivePolyGroups), MeshName = rm->GetName()](RealtimeMesh::FRealtimeMeshUpdateContext &UpdateContext,
+                                                                               RealtimeMesh::FRealtimeMeshSectionGroupSimple &SectionGroup) mutable {
+        for (const int32 PolyGroupIndex : ActivePolyGroups) {
+          const FRealtimeMeshSectionKey SectionKey = FRealtimeMeshSectionKey::CreateForPolyGroup(SectionGroup.GetKey(UpdateContext), PolyGroupIndex);
+          if (const auto Section = SectionGroup.GetSectionAs<RealtimeMesh::FRealtimeMeshSectionSimple>(UpdateContext, SectionKey)) {
+            const FRealtimeMeshSectionConfig SectionConfig(PolyGroupIndex);
+            Section->UpdateConfig(UpdateContext, SectionConfig);
+            Section->SetShouldCreateCollision(UpdateContext, true);
+          } else {
+            // This should not happen if poly-group streams are valid, but avoid hard ensures in the plugin.
+            LOG(ERROR_LL) << "RuntimeMeshBuilder: missing poly-group section " << SectionKey.ToString()
+                          << " Mesh=" << MeshName;
+          }
+        }
+      });
 
     UpdateBuilder.Commit(rm->GetMeshData());
 
