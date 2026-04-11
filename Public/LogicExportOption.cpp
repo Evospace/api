c80e527afb07dc69e0edeb6f494becd52c075d8d
wavelet-noise <ishellstrike@gmail.com>
2026-04-12 02:20:56 +0300

    cpp move

--- Public/LogicExportOption.cpp
+++
@@ -1,9 +0,0 @@
-#include "LogicExportOption.h"
-
-bool ULogicExportOption::DeserializeJson(TSharedPtr<FJsonObject> json) {
-  json_helper::TryFind(json, "Signal", Signal);
-  json_helper::TryGet(json, "Enabled", bEnabled);
-  json_helper::TryGet(json, "Label", Label);
-  json_helper::TryGet(json, "Tooltip", Tooltip);
-  return true;
-}
