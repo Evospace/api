8a71e6e0d59ba7edf8c9edf85b458b7a99310eb0
wavelet-noise <ishellstrike@gmail.com>
2026-04-12 19:45:46 +0300

    water amd fix test

--- Public/EngineDataSubsystem.cpp
+++ Public/EngineDataSubsystem.cpp
@@ -147,7 +147,6 @@ void UEngineDataSubsystem::ApplyData() const {
     const int32 renderTargetPoolMinMb = FMath::Max(Storage.RenderTargetPoolMinMb, 128);
 
     SetCVar(TEXT("r.AOGlobalDistanceField"), 0);
-    SetCVar(TEXT("r.Water.SingleLayer.Reflection"), 0);
     SetCVar(TEXT("r.Substrate"), 0);
     SetCVar(TEXT("r.GBufferFormat"), 1);
     SetCVar(TEXT("r.TextureStreaming"), 1);
