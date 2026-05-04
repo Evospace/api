0d6a457e9e184c88319ebb1f2cf039bd855c4b35
wavelet-noise <ishellstrike@gmail.com>
2026-05-04 17:34:02 +0300

    Revert "debug"
    This reverts commit 2e6664c4ab466d15e552aefe6db3d98816ec2588.

--- Public/ConveyorNetwork.h
+++ Public/ConveyorNetwork.h
@@ -4,7 +4,10 @@
 #include "NetworkBase.h"
 #include "ConveyorNetwork.generated.h"
 
+class ADimension;
 class UConveyorBlockLogic;
+class UBaseInventoryAccessor;
+class UInventoryAccess;
 
 UCLASS()
 class UConveyorNetwork : public UNetworkBase {
@@ -12,4 +15,48 @@ class UConveyorNetwork : public UNetworkBase {
   public:
   UPROPERTY(VisibleAnywhere)
   TArray<UConveyorBlockLogic *> Conveyors;
+
+  virtual void Tick() override;
+
+  // Rebuild static topology/cache after network is (re)constructed
+  void RebuildCache();
+
+  // Cached topology (stable between rebuilds)
+  UPROPERTY(VisibleAnywhere)
+  TArray<int32> NeighborIndex; // per conveyor index, -1 if receiver is not a conveyor
+
+  UPROPERTY(VisibleAnywhere)
+  TArray<UBaseInventoryAccessor *> ReceiverAccessors; // per conveyor
+
+  UPROPERTY(VisibleAnywhere)
+  TArray<int32> ReceiverId; // per conveyor, -1 if no receiver
+
+  UPROPERTY(VisibleAnywhere)
+  TArray<int32> PostOrder; // downstream-to-upstream processing order
+
+  UPROPERTY(VisibleAnywhere)
+  TArray<UInventoryAccess *> CachedInput; // per conveyor
+
+  UPROPERTY(VisibleAnywhere)
+  TArray<UInventoryAccess *> CachedOutput; // per conveyor
+
+  UPROPERTY(VisibleAnywhere)
+  TArray<UInventoryAccess *> ReceiverExternalInput; // per conveyor, only for non-conveyor receivers
+
+  struct FConveyorTickState {
+    bool InputEmpty = true;
+    bool OutputEmpty = true;
+    bool InputReady = false;
+    bool OutputReady = false;
+  };
+
+  TArray<FConveyorTickState> TickStates;
+  TArray<int32> ActivePostOrder;
+
+  // Working buffers reused between ticks (to avoid reallocations)
+  TArray<uint8> TmpVisited;
+  TArray<bool> Accept;
+  TArray<bool> OutClears;
+  TArray<UConveyorBlockLogic *> ChosenSenderByReceiver; // indexed by compact ReceiverId
+  int32 UniqueReceiverCount = 0;
 };
